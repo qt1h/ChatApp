029898a2515565aa2b119e94d67a77a5996f53f2
quentinproject <girardatquentin@gmail.com>
2023-12-15 14:05:36 +0100

    added models and views

--- Voxy/users/models.py
+++ Voxy/users/models.py
@@ -1,3 +1,24 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from django.utils import timezone
+import datetime
 # Create your models here.
+
+class User(models.Model):
+    username=models.CharField(max_length=50)
+    password=models.CharField(max_length=50)
+    publication_date=models.DateTimeField('date plubished')
+    def published_this_week(self):
+        if self.publication_date>= (timezone.now()-datetime.timedelta(days=7)):
+            return True
+        else:
+            return False
+    def __str__(self):
+        return self.username
+    
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    # Add additional fields related to the profile if needed
+
+    def __str__(self):
+        return self.user.username
--- Voxy/users/views.py
+++ Voxy/users/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
-# Create your views here.
+def sign_up(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')  # Redirect to your home page after successful sign-up
+    else:
+        form = UserCreationForm()
+    return render(request, 'sign_up.html', {'form': form})
+
+def log_in(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, request.POST)
+        if form.is_valid():
+            user = authenticate(request, username=form.cleaned_data['username'], password=form.cleaned_data['password'])
+            if user is not None:
+                login(request, user)
+                return redirect('home')  # Redirect to your home page after successful login
+    else:
+        form = AuthenticationForm()
+    return render(request, 'log_in.html', {'form': form})

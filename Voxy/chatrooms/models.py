5b3cfa13b19a5c3b436f315ddce1c5e72678ec84
Kbidule <romain.bomba@uha.fr>
2024-01-04 18:46:10 +0100

    chatromms
     implementation links

--- Voxy/chatrooms/models.py
+++ Voxy/chatrooms/models.py
@@ -1,3 +1,22 @@
+from django.contrib.auth.models import User
 from django.db import models
+from datetime import datetime
 
-# Create your models here.
+class ChatRoom(models.Model):
+    name = models.CharField(max_length=255)
+    participants = models.ManyToManyField(User)  # Liste des personnes ayant accès à la chat room
+
+    def __str__(self):
+        return self.name
+
+class Message(models.Model):
+    content = models.TextField()
+    sender = models.ForeignKey(User, on_delete=models.CASCADE)  # La personne qui a posté le message
+    timestamp = models.DateTimeField(default=datetime.now, blank=True)
+    chat_room = models.ForeignKey(ChatRoom, on_delete=models.CASCADE)  # La chat room à laquelle le message est associé
+
+    def __str__(self):
+        return f"{self.sender.username} - {self.content[:50]}"
+
+    class Meta:
+        ordering = ['timestamp']
--- Voxy/chatrooms/views.py
+++ Voxy/chatrooms/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
-
-def chatroom(request):
-    return render(request, 'chatroom.html')
+from .models import ChatRoom
+def home_view(request):
+    user = request.user
+    accessible_chatrooms = ChatRoom.objects.filter(participants=user)
+    return render(request, 'chatroom.html', {'user': user, 'accessible_chatrooms': accessible_chatrooms})

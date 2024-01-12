ad2fad857aef5735dcd429df2df4b06559090ef5
quentinproject <girardatquentin@gmail.com>
2024-01-12 20:55:27 +0100

    checked even more

--- Voxy/manage_emojis/models.py
+++ Voxy/manage_emojis/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Emoji(models.Model):
     name = models.CharField(max_length=255)
-    symbol = models.CharField(max_length=10)  # Stocker le symbole de l'emoji (ex. 😀)
+    symbol = models.CharField(max_length=10)  # Stock the emoji (ex. 😀)
 
     def __str__(self):
         return self.name

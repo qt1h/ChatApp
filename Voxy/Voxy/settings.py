3e548bfdac8fa6b643d659efcb30d7e43373b482
quentinproject <girardatquentin@gmail.com>
2024-01-12 21:47:19 +0100

    Last version without debug

--- Voxy/Voxy/settings.py
+++ Voxy/Voxy/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-*3%&u&i^eb=nsr9ggtsg8tmt!9zcc=cgye@*j2q0fu47&ca+8*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["kdibule.elitios.net" ,'localhost', '127.0.0.1']
 
